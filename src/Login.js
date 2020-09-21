50eb5db40ea9bd5d230a5918a52648879efd500f
AhadKhan98 <ahadkhan98@hotmail.com>
2020-09-21 15:18:38 +0500

    set up spotify auth and got a response back

--- src/Login.js
+++ src/Login.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './Login.css';
 
+import { getTokenFromUrl, loginURL } from './spotify';
+
 function Login() {
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
-            <a>LOGIN WITH SPOTIFY</a>
+            <a href={loginURL}>LOGIN WITH SPOTIFY</a>
+            {console.log(getTokenFromUrl())}
         </div>
     )
 };
---
+++ src/spotify.js
@@ -0,0 +1,29 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectURI = "http://localhost:3000/";
+
+const clientID = "563450ab8c35413faa269f9a24dee9b6";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    const parameters = window.location.hash.split('&');
+    let access_code = null;
+    for(let i=0; i < parameters.length; i++) {
+        let parts = parameters[i].split("=");
+        if (parts[0] === '#access_token') {
+            access_code = parts[1];
+            break;
+        }
+    }
+    return access_code;
+};
+
+export const loginURL = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+
