3f9cf1520b022f98ee0b0d435e7a0cd5510db310
AhadKhan98 <ahadkhan98@hotmail.com>
2020-09-21 15:46:29 +0500

    use of state to store token from response

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
+
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const _token = getTokenFromUrl();
+    window.location.hash = "";
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      { token ? <Player /> : <Login /> }
     </div>
   );
 }
