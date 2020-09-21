507f73479eeca56ac03b1c3afbd807355fa359f8
AhadKhan98 <ahadkhan98@hotmail.com>
2020-09-21 16:56:41 +0500

    using state for token and user

--- src/App.js
+++ src/App.js
@@ -5,26 +5,35 @@ import Login from './Login';
 import Player from './Player';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from './DataLayer';
 
 const spotifyAPI = new SpotifyWebApi();
 
 function App() {
 
-  const [token, setToken] = useState(null);
+  const [{user, token}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
+
     const _token = getTokenFromUrl();
     window.location.hash = "";
+
     if (_token) {
-      setToken(_token);
+
+      dispatch({type: 'SET_TOKEN', token: _token});
+
       spotifyAPI.setAccessToken(_token);
       spotifyAPI.getMe().then(user => {
-        console.log('PERSON>', user);
+        dispatch({type:'SET_USER', user});
       });
     }
   }, []);
 
+  console.log('USER>>>',user);
+  console.log('TOKEN>>>', token);
+
   return (
+
     <div className="app">
       { token ? <Player /> : <Login /> }
     </div>
