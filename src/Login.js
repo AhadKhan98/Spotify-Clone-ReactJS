8adc4f9d5582c97d63f1451e1f0774b8c9b2dbb7
AhadKhan98 <ahadkhan98@hotmail.com>
2020-09-29 14:59:53 +0500

    added instructions and credits

--- src/Login.js
+++ src/Login.js
@@ -8,6 +8,19 @@ function Login() {
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            <p>Spotify Clone App (Made By <a target="_blank" href="http://ahadzai.com/">Ahad Zai</a>) </p>
+            <p className="instructions"> 
+                Instructions: 
+                <br />
+                1) Login with your Spotify account.
+                <br />
+                2) Make sure you have a device that is online and available on Spotify.
+                <br />
+                3) The clone app will broadcast your selections straight to your primary device using Spotify's API.
+                <br /><br />
+                Note: The app itself does not play songs, you NEED a device that is online on spotify.
+            </p>
+
             <a href={loginURL}>LOGIN WITH SPOTIFY</a>
         </div>
     )
