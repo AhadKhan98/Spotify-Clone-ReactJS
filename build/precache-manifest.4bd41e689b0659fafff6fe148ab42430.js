880d952302f87fdb27512f328e29283c60df6b27
AhadKhan98 <ahadkhan98@hotmail.com>
2020-10-08 08:40:33 +0500

    fix spotify redirect uri and add firebase

---
+++ build/precache-manifest.4bd41e689b0659fafff6fe148ab42430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4f4b6f9bed84fb8dedc873d9ce5e99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bb1bb2e897381243a72",
+    "url": "/static/css/main.3d0c94cc.chunk.css"
+  },
+  {
+    "revision": "7875259ad9c4429b0656",
+    "url": "/static/js/2.3819a5f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3819a5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb1bb2e897381243a72",
+    "url": "/static/js/main.3c15839c.chunk.js"
+  },
+  {
+    "revision": "4360aa0156b88b96d485",
+    "url": "/static/js/runtime-main.2af6fe75.js"
+  }
+]);
